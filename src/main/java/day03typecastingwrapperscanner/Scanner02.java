4c58bd193c5b0c988b49af38b9da7974e622653b
Senem Aydın <dytsenemcelik@gmail.com>
2023-06-08 17:06:34 +0300

    java

---
+++ src/main/java/day03typecastingwrapperscanner/Scanner02.java
@@ -0,0 +1,17 @@
+package day03typecastingwrapperscanner;
+
+import java.util.Scanner;
+
+public class Scanner02 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your fullname");
+
+        String fullname = scan.nextLine();//To get String data from user use nextLine() method
+
+        System.out.println("The user's name is " + fullname);
+
+    }
+}
